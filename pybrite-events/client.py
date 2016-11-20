a15af64fa4dcce46e58109468996916fb5030d0a
Edward <me@edward.sh>
2016-11-20 15:33:44 -0500

    added config file,

--- pybrite-events/client.py
+++ pybrite-events/client.py
@@ -22,3 +22,9 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
+
+class ApiClient:
+    def __init__(self, auth_token):
+        self.auth_token = auth_token
+        
+    
